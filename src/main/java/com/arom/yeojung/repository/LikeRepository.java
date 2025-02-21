a927910061481e13104592e78eb632ec001b81ee
uechann <cab07036@naver.com>
2025-02-21 19:48:27 +0900

    feat(Like): Like 좋아요 관련 기능 구현

--- src/main/java/com/arom/yeojung/repository/LikeRepository.java
+++ src/main/java/com/arom/yeojung/repository/LikeRepository.java
@@ -3,5 +3,8 @@ package com.arom.yeojung.repository;
 import com.arom.yeojung.object.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUserIdAndDiaryId(Long userId, Long diaryId);
 }
--- src/main/java/com/arom/yeojung/service/LikeService.java
+++ src/main/java/com/arom/yeojung/service/LikeService.java
@@ -1,4 +1,46 @@
 package com.arom.yeojung.service;
 
-public class LikeService {
+import com.arom.yeojung.object.BaseTimeEntity;
+import com.arom.yeojung.object.Diary;
+import com.arom.yeojung.object.Like;
+import com.arom.yeojung.object.User;
+import com.arom.yeojung.object.dto.LikeDto;
+import com.arom.yeojung.repository.DiaryRepository;
+import com.arom.yeojung.repository.LikeRepository;
+import com.arom.yeojung.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService extends BaseTimeEntity {
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final DiaryRepository diaryRepository;
+
+    //좋아요 생성
+    public LikeDto createLike(LikeDto likeDto) {
+        User user = userRepository.findById(likeDto.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("not found user"));
+
+        Diary diary = diaryRepository.findById(likeDto.getDiaryId())
+                .orElseThrow(() -> new NoSuchElementException("not found diary"));
+
+        Like like = new Like();
+        like.setUser(user);
+        like.setDiary(diary);
+        like.setCreatedDate(getCreatedDate());
+        likeRepository.save(like);
+        return likeDto;
+    }
+
+    //좋아요 삭제
+    public String deleteLike(LikeDto likeDto) {
+        Like like = likeRepository.findByUserIdAndDiaryId(likeDto.getUserId(), likeDto.getDiaryId())
+                .orElseThrow(() -> new NoSuchElementException("not found like"));
+        likeRepository.delete(like);
+        return "success";
+    }
 }
