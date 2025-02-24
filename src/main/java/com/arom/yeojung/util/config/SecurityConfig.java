10dea081cbae2ee672e4d088ce84c9e2ff177921
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-24 19:07:18 +0900

    feat(login): CORS 설정

--- src/main/java/com/arom/yeojung/util/config/SecurityConfig.java
+++ src/main/java/com/arom/yeojung/util/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.arom.yeojung.util.config;
 import com.arom.yeojung.security.filter.JWTFilter;
 import com.arom.yeojung.security.filter.LoginFilter;
 import com.arom.yeojung.security.jwt.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -39,6 +44,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((cors)->cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        }));
 
         http
                 .csrf(AbstractHttpConfigurer::disable);
