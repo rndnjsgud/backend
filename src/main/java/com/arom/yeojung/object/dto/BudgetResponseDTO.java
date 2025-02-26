43dc16460fa8687d7df479e7ecdae21d84741cf9
rndnjsgud <00kwh@naver.com>
2025-02-26 18:43:26 +0900

    feat(Budget): 예산 관련 기능 구현

--- src/main/java/com/arom/yeojung/object/dto/BudgetResponseDTO.java
+++ src/main/java/com/arom/yeojung/object/dto/BudgetResponseDTO.java
@@ -3,15 +3,17 @@ package com.arom.yeojung.object.dto;
 import com.arom.yeojung.object.BudgetCategory;
 import com.arom.yeojung.object.BudgetType;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class BudgetResponseDTO {
     @NotNull
     private Long budgetId;
     private BudgetCategory budgetCategory;
     private String budgetName;
-    private Long BudgetAmount;
+    private Long budgetAmount;
     private BudgetType budgetType;
     private Long subPlanId;
 }
--- src/main/java/com/arom/yeojung/repository/SubPlanRepository.java
+++ src/main/java/com/arom/yeojung/repository/SubPlanRepository.java
@@ -1,4 +1,7 @@
 package com.arom.yeojung.repository;
 
-public interface SubPlanRepository {
+import com.arom.yeojung.object.SubPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubPlanRepository extends JpaRepository<SubPlan, Long> {
 }
