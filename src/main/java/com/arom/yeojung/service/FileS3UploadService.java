0659e3674951b6d7b652e64e979a167847f6b5d2
uechann <cab07036@naver.com>
2025-02-28 03:15:36 +0900

    refactor(fileUpload): Byte에서 Inputstream으로 수정하여 메모리 절약, 파일 크기 제한 10MB로 설정

--- src/main/java/com/arom/yeojung/service/FileS3UploadService.java
+++ src/main/java/com/arom/yeojung/service/FileS3UploadService.java
@@ -3,6 +3,8 @@ package com.arom.yeojung.service;
 
 import com.arom.yeojung.object.File;
 import com.arom.yeojung.repository.FileRepository;
+import com.arom.yeojung.util.exception.CustomException;
+import com.arom.yeojung.util.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +17,12 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
 public class FileS3UploadService {
-
+    private static final long MAX_FILE_SIZE = 10 * 1024 * 1024; //10MB 최대 파일 사이즈
     private final String bucketName = "yeojung-bucket";
     private final S3Client s3Client;
     private final FileService fileService;
@@ -37,18 +40,23 @@ public class FileS3UploadService {
     }
 
     public File uploadAndSaveFile(MultipartFile file) {
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new CustomException(ErrorCode.FILE_SIZE_EXCEED);
+        }
+
         String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
         String fileUrl = "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + fileName;
 
-        try {
+        try (InputStream inputStream = file.getInputStream()) { //inputStream 사용
             s3Client.putObject(PutObjectRequest.builder()
                             .bucket(bucketName)
                             .key(fileName)
+                            .contentType(file.getContentType())
                             .build(),
-                    RequestBody.fromBytes(file.getBytes()));
+                    RequestBody.fromInputStream(inputStream, file.getSize())); //메모리 사용량 최적화
             return fileService.save(fileName, fileUrl);
         } catch (IOException e) {
-            throw new RuntimeException("S3 Failed to upload file", e);
+            throw new CustomException(ErrorCode.FILE_UPLOAD_FAILED);
         }
     }
 }
