e17670a3c85221cad0bcc832922bcea86e9f5047
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-02 18:16:13 +0900

    refact(login): 커스텀 익셉션 반환

--- src/main/java/com/arom/yeojung/util/exception/ErrorCode.java
+++ src/main/java/com/arom/yeojung/util/exception/ErrorCode.java
@@ -14,7 +14,12 @@ public enum ErrorCode {
 
   INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
-  ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다.");
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+
+  // User
+
+  EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 사용자입니다.");
+
 
   private final HttpStatus status;
   private final String message;
