8f12cb06fd862385dd15add1f5b526117bcd3d8b
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-07 03:03:51 +0900

    fix : 충돌 해결

--- src/main/java/com/arom/yeojung/util/config/SwaggerConfig.java
+++ src/main/java/com/arom/yeojung/util/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.arom.yeojung.util.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +14,30 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 @Configuration
 public class SwaggerConfig {
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-        .info(new Info()
-            .title("My API Docs")
-            .version("1.0")
-            .description("API 문서 설명"))
-        .servers(List.of(
-            new Server().url("http://localhost:8080").description("Local 개발 서버"),
-            new Server().url("https://3.35.38.147:8080").description("운영 서버")
-        ));
-  }
+    SecurityScheme apiKey = new SecurityScheme()
+            .type(Type.HTTP)
+            .in(In.HEADER)
+            .name("Authorization")
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList("Bearer Token");
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .info(new Info()
+                        .title("My API Docs")
+                        .version("1.0")
+                        .description("API 문서 설명"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local 개발 서버"),
+                        new Server().url("https://3.35.38.147:8080").description("운영 서버")
+                ));
+    }
 }
 
 
